aaee53e8e39aee24d4bb41902531954fb258fe03
Luis Leguizamón <lleguizamonl@gmail.com>
2022-07-05 18:07:05 -0400

    feat: Select php version from input

--- phpversion.sh
+++ phpversion.sh
@@ -1,4 +1,16 @@
-PHPVERSION=7.4
+#PHPVERSION=7.4
+read -p "Enter PHP version (2 to select 7.2, 4 to select 7.4, 8 to select 8.0): " PHPVERSION
+case $PHPVERSION in
+        "2")
+        PHPVERSION=7.2
+        ;;
+        "4")
+        PHPVERSION=7.4
+        ;;
+        "8")
+        PHPVERSION=8.0
+        ;;
+esac
 echo $PHPVERSION
 sudo update-alternatives --set php /usr/bin/php$PHPVERSION
 sudo update-alternatives --set phar /usr/bin/phar$PHPVERSION

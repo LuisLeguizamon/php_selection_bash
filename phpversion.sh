7a50f30cf67a0449fc86c5873cbaa65f4bd7cf92
Luis Leguizamón <lleguizamonl@gmail.com>
2022-07-05 17:48:28 -0400

    feat: Create phpversion.sh

---
+++ phpversion.sh
@@ -0,0 +1,8 @@
+PHPVERSION=7.4
+echo $PHPVERSION
+sudo update-alternatives --set php /usr/bin/php$PHPVERSION
+sudo update-alternatives --set phar /usr/bin/phar$PHPVERSION
+sudo update-alternatives --set phar.phar /usr/bin/phar.phar$PHPVERSION
+sudo update-alternatives --set phpize /usr/bin/phpize$PHPVERSION
+sudo update-alternatives --set php-config /usr/bin/php-config$PHPVERSION
+php -v
